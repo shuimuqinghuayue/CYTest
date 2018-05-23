75d7e68c547fea2865dbbfe0acef76695ab3d3d4
changyue <changyue206530@sogou-inc.com>
2018-05-23 17:20:21 +0800

    hhh

---
+++ CYChannelScrollView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CYChannelScrollView
+//
+//  Created by 悦 on 2018/5/23.
+//  Copyright © 2018年 悦. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CYChannelScrollView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CYChannelScrollView
+//
+//  Created by 悦 on 2018/5/23.
+//  Copyright © 2018年 悦. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
